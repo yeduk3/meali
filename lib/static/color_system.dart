d21cba16ddc0412abf48dd1dee9466367193d2e5
이용규 <gyu@iyong-gyuui-MacBookAir.local>
2025-01-03 23:34:08 +0900

    FEAT: Image Comment Test4

--- lib/static/color_system.dart
+++ lib/static/color_system.dart
@@ -8,19 +8,58 @@ class ColorSystem {
   /// The gray primary color and swatch for EDS.
   ///
   /// ![](https://raw.githubusercontent.com/yeduk3/meali/refs/heads/dev/lib/static/colorpreview/Gray.png)
-  static const MaterialColor gray = MaterialColor(_grayPrimaryValue, <int, Color>{
-    50: Color(0xFFFAFAFA),
-    100: Color(0xFFF5F5F5),
-    200: Color(0xFFE9E9E9),
-    300: Color(0xFFDBDBDB),
-    400: Color(0xFFBABABA),
-    500: Color(_grayPrimaryValue),
-    600: Color(0xFF737373),
-    700: Color(0xFF545454),
-    800: Color(0xFF3E3E3E),
-    900: Color(0xFF1A1E22),
-  });
+  static const MaterialColor gray = MaterialColor(
+    _grayPrimaryValue,
+    <int, Color>{
+      50: Color(0xFFFAFAFA),
+      100: Color(0xFFF5F5F5),
+      200: Color(0xFFE9E9E9),
+      300: Color(0xFFDBDBDB),
+      400: Color(0xFFBABABA),
+      500: Color(_grayPrimaryValue),
+      600: Color(0xFF737373),
+      700: Color(0xFF545454),
+      800: Color(0xFF3E3E3E),
+      900: Color(0xFF1A1E22),
+    },
+  );
   static const int _grayPrimaryValue = 0xFF8E8E8E;
+
+  /// The black transparency primary color and swatch for EDS.
+  static const MaterialColor blackTransparency = MaterialColor(
+    _blackTransparencyPrimaryValue,
+    <int, Color>{
+      5: Color(0x0c000000),
+      10: Color(0x19000000),
+      20: Color(0x33000000),
+      30: Color(0x4c000000),
+      40: Color(0x66000000),
+      50: Color(_blackTransparencyPrimaryValue),
+      60: Color(0x99000000),
+      70: Color(0xb3000000),
+      80: Color(0xcc000000),
+      90: Color(0xe6000000),
+    },
+  );
+  static const int _blackTransparencyPrimaryValue = 0x80000000;
+
+  /// The white transparency primary color and swatch for EDS.
+  static const MaterialColor whiteTransparency = MaterialColor(
+    _whiteTransparencyPrimaryValue,
+    <int, Color>{
+      5: Color(0x0cffffff),
+      10: Color(0x19ffffff),
+      20: Color(0x33ffffff),
+      30: Color(0x4cffffff),
+      40: Color(0x66ffffff),
+      50: Color(_whiteTransparencyPrimaryValue),
+      60: Color(0x99ffffff),
+      70: Color(0xb3ffffff),
+      80: Color(0xccffffff),
+      90: Color(0xe6ffffff),
+    },
+  );
+  static const int _whiteTransparencyPrimaryValue = 0x80ffffff;
 
   /// 0xFF191F28
   static const Color gray01 = Color(0xFF191F28);
